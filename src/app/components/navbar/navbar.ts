114f47c80814ca676e957d319d424c8980ef7b17
james-qs <75996925@continental.edu.pe>
2026-06-16 15:00:47 -0500

    feat: responsive móvil - navbar hamburguesa y mapa adaptable

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth';
@@ -12,4 +12,13 @@ import { AuthService } from '../../services/auth';
 })
 export class NavbarComponent {
   auth = inject(AuthService);
+  menuAbierto = signal(false);
+
+  toggleMenu() {
+    this.menuAbierto.set(!this.menuAbierto());
+  }
+
+  cerrarMenu() {
+    this.menuAbierto.set(false);
+  }
 }
